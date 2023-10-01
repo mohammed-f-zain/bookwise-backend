1efd006f30e0d7b629b2bc1a6b264011bf7589ba
mohammed saeed <mohammed.f.zain@gmail.com>
2023-10-01 19:32:16 +0300

    image

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import authorRoutes from "./routes/authorRoutes.js"
 const app = express();
 app.use(cors());
 dotenv.config();
-app.use(express.json());
+app.use(express.json({limit:"30mb"}));
 
 app.use("/user", userRoutes);
 app.use("/books", booksRoutes);
